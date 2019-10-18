0a294528417ee42149d0f74596f4033da62cfe84
Shivam Mani Tripathi <31733809+geekcodershivam@users.noreply.github.com>
2019-10-18 07:57:58 +0530

    deploy

---
+++ build/precache-manifest.e4f50c51e5ea9b3c6e41f64eabcac9ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec18d39d8add9df0333ec05e9cdbff96",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2624e855177811e33cad",
+    "url": "/static/css/main.480ba89e.chunk.css"
+  },
+  {
+    "revision": "1c70485e7cfd1e1a9bac",
+    "url": "/static/js/2.f76bf850.chunk.js"
+  },
+  {
+    "revision": "2624e855177811e33cad",
+    "url": "/static/js/main.64b43e61.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "a0783f5895367ffabbe18ecef059ea48",
+    "url": "/static/media/bg.a0783f58.png"
+  }
+]);
